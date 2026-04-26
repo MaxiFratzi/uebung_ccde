bb7c4d1500aeb5e30266bd35e5d8ffcd2e50a205
MaxiFratzi <maxdraxl@tsn.at>
2026-04-26 22:57:37 +0200

    Add multiplication-function

--- calc.py
+++ calc.py
@@ -4,6 +4,10 @@ def add(a, b):
 def sub(a, b):
     return a-b
 
+def mult(a, b):
+    return a*b
+
 if __name__ == "__main__":
     print("5 + 3 =", add(5, 3))
     print("5 - 3 =", sub(5, 3))
+    print("5 * 3 =", mult(5, 3))
